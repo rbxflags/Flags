6006b1e0aa6d603afa492bce25c9ea1f3c0140c6
ExponentialWorkload <90570076+Exponential-Workload@users.noreply.github.com>
2023-06-16 16:50:09 +0200

    fix: Restructure flag list to match new type

---
+++ flaglist.d.ts
@@ -0,0 +1,56 @@
+
+type FileHash = {
+  /** Algorithm, throw err if MD5 */
+  algorithm: string,
+  /** Hash Digest */
+  digest: string,
+} | {
+  /** No Algo */
+  algorithm: 'none',
+  /** Hash Digest */
+  digest: undefined | null,
+}
+type File = {
+  /** File Route relative to {@link FlagItem.base} */
+  f: string,
+  /** File Hash */
+  h: FileHash,
+}
+type Feature<T extends string> = {
+  /** Human-Readable Name */
+  name: string,
+  /** Question */
+  question: string,
+  /** Options */
+  options: Record<T, File[]>,
+  /** Default Option */
+  default: T[],
+  /** Multiple Choice */
+  multiple: true,
+  /** Minimum Choices */
+  min: number,
+  /** Maximum Choices */
+  max: number,
+} | {
+  /** Human-Readable Name */
+  name: string,
+  /** Options */
+  options: Record<T, File[]>,
+  /** Default Option */
+  default: T,
+  /** Multiple Choice */
+  multiple: false,
+}
+type FlagItem = {
+  /** Base URL - e.g. https://rfo.sh/flags/ */
+  baseurl: string,
+  /** Is it enabled by default */
+  default: boolean,
+  /** Human-Readable Name */
+  name: string,
+  /** Base Flags */
+  base: File[],
+  /** Feature List */
+  features: Feature<any>[],
+}
+type FlagList = Record<string, FlagItem>
